ca09b430e93513d957ab96a61c6835e17be4e5eb
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2023-10-04 23:41:26 +0100

    second commit

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ from qoutes import funny_qoutes
 app = Flask(__name__)
 
 
-@app.route("/api/funny")
+@app.route("/api/funny/single")
 def serve_funny_qoute():
     qoutes = funny_qoutes()
     nr_of_qoutes=len(qoutes)
     selected = qoutes[random.randint(0,nr_of_qoutes -1)]
     return jsonify(selected)
+
+
+@app.route('/api/funny/all')
+def all():
+    qoutes=funny_qoutes()
+    return qoutes
 
 if __name__=="__main__":
     app.run(debug=True)
